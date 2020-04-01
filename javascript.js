e2b654b6e80d9730769da1f138ea6bdec01be86a
Luke-McShane <lukejsmcshane@gmail.com>
2020-04-01 20:43:05 +1100

    Created Modal popup. Save button functionality needs adding

--- javascript.js
+++ javascript.js
@@ -1,5 +1,31 @@
-//document.getElementById("generateQuote").addEventListener("click", quoteGen);
-//document.getElementById("selectBooks").addEventListener("click", bookSelect);
+document.getElementById("generateQuote").addEventListener("click", quoteGen);
+document.getElementById("selectBooks").addEventListener("click", bookSelect);
+document.getElementById("saveButton").addEventListener("click", saveSelection);
+document.getElementById("cancelButton").addEventListener("click", cancelSelection);
+
+// Get the modal
+var modal = document.getElementsByClassName("modal")[0];
+
+
+let totalQuotes = 0;
+let mainChunks;
+let naughtyArray = [];
+
+let getFontSize = (textLength) => {
+    //console.log(textLength);
+    let textSize = 1.5;
+    if (textLength < 325) return `${textSize}vw`;
+
+    const maxLen = 325;
+    const maxSize = 1.5;
+    console.log(textLength);
+    textSize = (maxLen/textLength) * maxSize;
+    if (textSize < 1) textSize = 1;
+    console.log(textSize);
+    console.log('');
+    return `${textSize}em`
+  }
+
 function readTextFile(file){
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
@@ -16,17 +42,16 @@ function readTextFile(file){
     }
     rawFile.send(null);
 }
-readTextFile("http://127.0.0.1:5501/test.txt");
+//readTextFile("http://127.0.0.1:5501/test2.txt");
+readTextFile("http://127.0.0.1:5501/allClippings.txt");
 
 function parseClippings(text) {
-    let mainChunks = text.split("==========");
-    //console.log(mainChunks);
+    mainChunks = text.split("==========");
+    totalQuotes = mainChunks.length-1;
     for(let i=0; i<mainChunks.length;i++) {
-       //mainChunks[i] = mainChunks[i].split(/(\r\n|\n|\r)/gm);
-       mainChunks[i] = mainChunks[i].split("\n");
-     
+       mainChunks[i] = mainChunks[i].split("\n");   
        for(let j=0; j<mainChunks[i].length; j++) {
-        if ( mainChunks[i][j].includes("- Your Highlight on page")){
+        if ( mainChunks[i][j].includes("- Your Highlight on page")) {
             mainChunks[i].splice(j, 1);
          } else if (mainChunks[i][j].length <= 4) {
             mainChunks[i].splice(j, 1);
@@ -34,44 +59,42 @@ function parseClippings(text) {
         }
     }
     mainChunks.pop();
-    //console.log(mainChunks);
     for(let i=0; i<mainChunks.length; i++){
-        //console.log(mainChunks[i][0]);
         mainChunks[i][0] = mainChunks[i][0].split("(");
-        //console.log(mainChunks[i][0][1].substring(0, mainChunks[i][0][1].length-2));
-        mainChunks[i][0][1] = mainChunks[i][0][1].substring(0, mainChunks[i][0][1].length-2);
-        //console.log(mainChunks[i][0]);
-}
-console.log(mainChunks[1][2]);
-document.getElementById("quote").textContent = mainChunks[2][2];
-document.getElementById("author").textContent = mainChunks[2][0][0];
-document.getElementById("book").textContent = mainChunks[2][0][1];
-    //console.log(mainChunks[1].split(/(\r\n|\n|\r)/gm));
-    
-    // mainChunks.forEach(chunk => {
-    //     let secondSplit = chunk.split(/(\r\n|\n|\r)/gm);
-    //     //secondSplit = secondSplit.replace(/(\r\n|\n|\r)/gm, "<br />");
-       
-    //     // secondSplit.forEach(element => {
-    //     //     element = element.replace(/(\r\n|\n|\r)/gm, "<br />");
-    //     // })
-    //     console.log(secondSplit);
-    //     for(let i=0; i<secondSplit.length; i++) {
-    //         //console.log(secondSplit[i]);
-    //         //console.log(`${secondSplit[i]} Length: ${secondSplit[i].length}`)
-    //         //console.log(i);
-    //         //console.log(typeof(secondSplit[i]));
-    //         // if (secondSplit[i].length <= 4 || secondSplit[i].includes("- Your Highlight on page")) {
-    //         //if (secondSplit[i].includes("Your Highlight") || (secondSplit[i].length <= 4)) {
-    //         console.log(`Item: ${secondSplit[i]} Length: ${secondSplit[i].length}`)
-    //         if ((secondSplit[i].length <= 4)) {
-    //             // console.log(secondSplit[i].length);
-    //             secondSplit.splice(i,1);
-    //         };
-    //         if((secondSplit[i].match(/^[0-9a-zA-Z]+$/))) secondSplit.splice(secondSplit[i], 1);
-    //     }
-    //     //console.log(secondSplit);
-    // })
-    
+        if(mainChunks[i][0][1].includes(',')) {
+            mainChunks[i][0][1] = mainChunks[i][0][1].split(',');
+            mainChunks[i][0][1][1]= mainChunks[i][0][1][1].substring(0, mainChunks[i][0][1][1].length-2).trim();
+            mainChunks[i][0][1] = `${mainChunks[i][0][1][1]} ${mainChunks[i][0][1][0]}`; 
+        } else mainChunks[i][0][1] = mainChunks[i][0][1].substring(0, mainChunks[i][0][1].length-2);
+    }   
 
+    quoteGen();
+    populateModal();
+}
+
+function quoteGen() {
+    let currentQuote = Math.floor(Math.random() * totalQuotes);
+    let quote = document.getElementById("quote");
+    //console.log(mainChunks[currentQuote]);
+    quote.textContent = mainChunks[currentQuote][2];
+    quote.style.fontSize = getFontSize(quote.textContent.length);
+    document.getElementById("author").textContent = mainChunks[currentQuote][0][0];
+    document.getElementById("book").textContent = mainChunks[currentQuote][0][1];
+}
+
+function populateModal() {
+    let textDiv = document.getElementById("modalTextDiv");
+
+}
+
+function bookSelect () {
+    modal.style.display = "flex";
+}
+
+function saveSelection() {
+
+}
+
+function cancelSelection() {
+    modal.style.display = "none";
 }
