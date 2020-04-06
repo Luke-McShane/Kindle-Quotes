80774bb66ff63ff7b20692018b6a583f8143bd2c
Luke-McShane <lukejsmcshane@gmail.com>
2020-04-07 08:56:22 +1000

    Fixed bug pertaining to 'quote' being undefinined. Cleaning up code. Pushing before testing if I'm able to reduce allowedBook.. arrays to 1, rather than having 3 separate ones.

--- javascript.js
+++ javascript.js
@@ -8,15 +8,13 @@ document.getElementById("buttonSelectAll").addEventListener("click", selectAll);
 document.getElementById("buttonDeselectAll").addEventListener("click", deselectAll);
 
 let modal = document.getElementsByClassName("modal")[0];
-let bannedBooks = [];
-let bannedBooksModal = []
+let allowedBooks = [];
+let allowedBooksModal = []
 let quotes = [];
 let quotesFiltered = [];
 let chunkedText;
 let book = "";
 let author = "";
-
-
 
 function readTextFile(file){
 
@@ -57,11 +55,12 @@ function parseText(text) {
         // Changing the order if the author's firstname is preceded by their lastname.
         if(chunkedText[i][0][1].includes(',')) {
             chunkedText[i][0][1] = chunkedText[i][0][1].split(',');
-            chunkedText[i][0][1][1]= chunkedText[i][0][1][1].substring(0, chunkedText[i][0][1][1].length-2).trim();
+            chunkedText[i][0][1][1]= chunkedText[i][0][1][1].trim().replace(')', '');
             chunkedText[i][0][1] = `${chunkedText[i][0][1][1]} ${chunkedText[i][0][1][0]}`;
 
         // Else we simply remove the closing bracket from the string.
-        } else chunkedText[i][0][1] = chunkedText[i][0][1].substring(0, chunkedText[i][0][1].length-2);
+        } else {console.log(chunkedText[i][0][1].trim().replace(')', ''));
+            chunkedText[i][0][1] = chunkedText[i][0][1].trim().substring(0, chunkedText[i][0][1].length-2);}
 
         quotes.push({"author": chunkedText[i][0][1], "book": chunkedText[i][0][0], "quote": chunkedText[i][2]})
         quotesFiltered.push({"author": chunkedText[i][0][1], "book": chunkedText[i][0][0], "quote": chunkedText[i][2]})
@@ -73,13 +72,22 @@ function parseText(text) {
     }
 
 function quoteGen() {
-    let currentQuote = Math.floor(Math.random() * quotesFiltered.length-1);
+    let currentQuote = Math.floor(Math.random() * quotesFiltered.length);
     let quote = document.getElementById("quote");
+    console.log(quotesFiltered);
+    let quoteText = quotesFiltered[currentQuote].quote;
+    let firstChar = quoteText[0];
+    let lastChar = quoteText[quotesFiltered[currentQuote].quote.length-2]
+    
+    if ((firstChar == firstChar.toLowerCase() && firstChar != firstChar.toUpperCase()) ||
+            ((firstChar != '“' && firstChar != '"') && !firstChar.match(/^[a-z0-9]+$/i))) {
+        quote.textContent = `"..${quoteText}`
+    } else quote.textContent = `"${quoteText}`;
+    
+    if (lastChar != '.') {
+        quote.textContent = `${quote.textContent}.."`
+    } else quote.textContent = `${quote.textContent}"`
 
-    //console.log(quotesFiltered);
-    quote.textContent = `"${quotesFiltered[currentQuote].quote}"`;
-    //quote.style.fontSize = getFontSize(quote.textContent.length);
-    // quote.style.fontSize = "2em";
     document.getElementById("authorBook").textContent = quotesFiltered[currentQuote].author +
             ": " + quotesFiltered[currentQuote].book;
     
@@ -93,31 +101,27 @@ function checkboxChange(checkboxElement) {
 
     let thisArray = document.getElementsByClassName(checkboxElement.classList[1]);
     let book = thisArray[2].innerText.replace(/ /g, '');
-    console.log(`bannedBooksModal before action: ${bannedBooksModal}`);
 
     if(!thisArray[0].checked) {
-        if(bannedBooksModal.includes(book)) {
-            console.log(`Unchecked...   Book: ${book} bannedBooksModal: ${bannedBooksModal} index: ${bannedBooksModal.indexOf(book)}`);
-            bannedBooksModal.splice(bannedBooksModal.indexOf(book), 1);
+        if(allowedBooksModal.includes(book)) {
+            console.log(`Unchecked...   Book: ${book} allowedBooksModal: ${allowedBooksModal} index: ${allowedBooksModal.indexOf(book)}`);
+            allowedBooksModal.splice(allowedBooksModal.indexOf(book), 1);
         }
     } 
-    else if (thisArray[0].checked && !bannedBooksModal.includes(book)) {
-        bannedBooksModal.push(book);
-        console.log(`Checked...   Book: ${book} bannedBooksModal: ${bannedBooksModal}`);
+    else if (thisArray[0].checked && !allowedBooksModal.includes(book)) {
+        allowedBooksModal.push(book);
+        console.log(`Checked...   Book: ${book} allowedBooksModal: ${allowedBooksModal}`);
     }
-    console.log(`bannedBooksModal after action: ${bannedBooksModal}`);
-    console.log('');
 
 }
 
 function bookSelect () {
 
     modal.style.display = "flex";
-    bannedBooksModal = bannedBooks;
-    //bannedBooks = bannedBooksModal;
-    //console.log(quotes.length);
+    allowedBooksModal = allowedBooks;
     let uniqueBooks = Array.from(new Set(quotes.map(item => item.book)))
         .map(book => {
+            allowedBooks
             return {
                 author: quotes.find(item => item.book == book).author,
                 book: book
@@ -129,9 +133,13 @@ function bookSelect () {
 }
 
 function tableGen(length, uniqueBooks) {
-
+    console.log("WAKA");
     let table = document.getElementById('modalTable');
-
+    
+    let numRows = Array.from(document.getElementsByClassName('inputModal')).length;
+    console.log(numRows);
+    if (numRows > 0) return;
+    
     for(let i = 0; i < length; i++) {
         let row = document.createElement("tr");
         let checkboxCell = document.createElement("td");
@@ -140,7 +148,7 @@ function tableGen(length, uniqueBooks) {
         let bookCell = document.createElement("td");
 
         input.type = "checkbox";
-        input.checked = 0;
+        input.checked = 1;
 
         input.id = `inputElement${i}`;
         authorCell.id = `authorElement${i}`;
@@ -171,23 +179,24 @@ function tableGen(length, uniqueBooks) {
 }
 
 function saveSelection() {
-    if (bannedBooksModal.length === 0) {
+    if (allowedBooksModal.length === 0) {
         alert("Please select at least one book.")
     } else {
-        bannedBooks = bannedBooksModal;
-        let bannedBooksTemp = [];
+        console.log(`Saved: ${allowedBooksModal}`);
+        allowedBooks = allowedBooksModal;
+        let allowedBooksTemp = [];
 
-        bannedBooksModal.forEach(element => {
+        allowedBooksModal.forEach(element => {
             //console.log(element);
-            bannedBooksTemp.push(element.replace(/ /g, ''));
+            allowedBooksTemp.push(element.replace(/ /g, ''));
         });
-        //console.log(`Banned Books Temp: ${bannedBooksTemp}`);
-        //console.log(`Quote: ${quotes[0].book.replace(/ /g, '')} In banedBooksTemp: ${bannedBooksTemp.includes(quotes[0].book.replace(/ /g, ''))}`)
+        //console.log(`allowed Books Temp: ${allowedBooksTemp}`);
+        //console.log(`Quote: ${quotes[0].book.replace(/ /g, '')} In banedBooksTemp: ${allowedBooksTemp.includes(quotes[0].book.replace(/ /g, ''))}`)
         quotesFiltered = quotes.filter(function(obj) {
 
             //console.log(`Object.book.trim(): "${obj.book.replace(/ /g, '')}"  element: "${element.replace(/ /g, '')}"   Comparison: ${obj.book.replace(/ /g, '') === element.replace(/ /g, '')}`);
             
-            return bannedBooksTemp.includes(obj.book.replace(/ /g, ''));
+            return allowedBooksTemp.includes(obj.book.replace(/ /g, ''));
         });
             
         
@@ -201,8 +210,38 @@ function saveSelection() {
 
 function cancelSelection() {
     modal.style.display = "none";
-    //console.log(bannedBooksModal);
-    bannedBooksModal = bannedBooks;
+    console.log(`before process: ${allowedBooks}`);    
+        allowedBooksModal = allowedBooks;
+    let allowedBooksTemp = [];
+    allowedBooksModal.forEach(element => {
+        allowedBooksTemp.push(element.replace(/ /g, ''));
+    });
+    //console.log(allowedBooksTemp);
+    Array.from(document.body.getElementsByClassName("inputModal")).forEach(element => {
+        let tableRow = document.getElementsByClassName(element.classList[1]);
+        //console.log(tableRow)
+        let book = tableRow[2].innerText.replace(/ /g, '');
+
+        // if (element.checked && !allowedBooksTemp.includes(book)) {
+        //     element.checked = 0;
+        // } else if (!element.checked && allowedBooksTemp.includes(book)) element.checked = 1;
+
+        //console.log(`allowedBooks: ${allowedBooks} book: ${book}`);
+
+        // if(!thisArray[0].checked) {
+        //     if(allowedBooksModal.includes(book)) {
+        //         console.log(`Unchecked...   Book: ${book} allowedBooksModal: ${allowedBooksModal} index: ${allowedBooksModal.indexOf(book)}`);
+        //         allowedBooksModal.splice(allowedBooksModal.indexOf(book), 1);
+        //     }
+        // } 
+        // else if (thisArray[0].checked && !allowedBooksModal.includes(book)) {
+        //     allowedBooksModal.push(book);
+        //     console.log(`Checked...   Book: ${book} allowedBooksModal: ${allowedBooksModal}`);
+        // }   
+    });
+    console.log(`After process: ${allowedBooks}`);
+    // console.log(quotesFiltered);
+    // console.log();
     quotes = [];
 };
 
@@ -217,10 +256,10 @@ function selectAll() {
     
         //console.log(book);
 
-        if(bannedBooksModal.includes(book)) {console.log(`List already includes this book! ${bannedBooksModal}`);console.log(bannedBooksModal);return;   }
-        else {bannedBooksModal.push(book); console.log(`Book added to list! ${bannedBooksModal}`);console.log(bannedBooksModal);}
+        if(allowedBooksModal.includes(book)) {console.log(`List already includes this book! ${allowedBooksModal}`);console.log(allowedBooksModal);return;   }
+        else {allowedBooksModal.push(book); console.log(`Book added to list! ${allowedBooksModal}`);console.log(allowedBooksModal);}
     });
-    //console.log(bannedBooksModal);
+    console.log(allowedBooksModal);
 }
 
 function deselectAll() {
@@ -234,11 +273,11 @@ function deselectAll() {
     
         //console.log(book);
 
-        if(bannedBooksModal.includes(book)) {bannedBooksModal.splice(bannedBooksModal.indexOf(book), 1);}
-            //console.log(`Book removed! ${bannedBooksModal}`); console.log(bannedBooksModal); console.log(bannedBooksModal);}
-        else {console.log(`Book not in list! ${bannedBooksModal}`); console.log(bannedBooksModal); return; }
+        if(allowedBooksModal.includes(book)) {allowedBooksModal.splice(allowedBooksModal.indexOf(book), 1);}
+            //console.log(`Book removed! ${allowedBooksModal}`); console.log(allowedBooksModal); console.log(allowedBooksModal);}
+        else { return; } //console.log(`Book not in list! ${allowedBooksModal}`); console.log(allowedBooksModal);
     });
-    //console.log(bannedBooksModal);
+    console.log(allowedBooksModal);
 }
 
 readTextFile("http://127.0.0.1:5501/allClippings.txt");
@@ -263,3 +302,11 @@ readTextFile("http://127.0.0.1:5501/allClippings.txt");
 //Use this: 'Object.fromEntries(mainChunks)' to simplify the initial parsing process.
 
 //Improve look and feel of landing page.
+
+//On Modal Cancel I must reset the buttons to what is in allowedBooks so that, when reopening, only the actually selected
+// books are ticked.
+
+//Find where books are being added to allowedBooks even when cancelled is pressed, for books should only be added to
+// this list after Save is clicked.
+
+//Add document.onReady JS equivalent to generate modal so that it isn't regerated each time you click the modal.
